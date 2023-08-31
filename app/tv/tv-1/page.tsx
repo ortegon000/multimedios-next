229ecb5adc3b93b999a95743cedcdfce5900166a
Eduardo Ortega <ortegon000@hotmail.com>
2023-08-30 18:22:26 -0600

    responsive tv 1

--- app/tv/tv-1/page.tsx
+++ app/tv/tv-1/page.tsx
@@ -3,21 +3,28 @@ import CircleLeft from '../../components/circle-left'
 import React from 'react'
 import Button from '../../components/button'
 
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Multimedios - Tv 1',
+  description: 'Presentación multimedios'
+}
+
 export default function Tv1 (): React.JSX.Element {
   return (
     <ContentLayout section='tv 1' background='3'>
       <div>
         <CircleLeft image='/images/tv/circle-left-1.png' />
 
-        <section className='tv-d1 mx-auto max-w-4xl mt-10 flex justify-end items-center'>
-          <div className='w-7/12'>
-            <div className='flex w-full'>
+        <section className='tv-d1 mx-auto max-w-4xl mt-4 flex justify-center md:justify-end items-center px-2'>
+          <div className='w-full max-w-lg bg-grayLight/50 backdrop-blur rounded-md p-2'>
+            <div className='flex w-full gap-2'>
               <img
                 src='/images/logo.png'
                 alt=''
                 className='w-32 object-contain animate__animated animate__bounce animate__repeat-2'
               />
-              <div className='w-full p-4'>
+              <div className='w-full'>
                 <h1 className='text-3xl font-bold'>Televisión en vivo.</h1>
                 <p className='text-2xl mt-4'>
                   Noticias, Entretenimiento y Deportes
@@ -30,7 +37,7 @@ export default function Tv1 (): React.JSX.Element {
               </div>
             </div>
 
-            <div className='flex gap-4 mt-4'>
+            <div className='flex flex-wrap gap-4 mt-4'>
               <Button
                 url='/tv/tv-2'
                 text='COBERTURA'
